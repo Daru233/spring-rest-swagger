7b3ba49fcdd35c0e2554c9618f8938a6ae2427cd
Michael Malto <michaeldm233@gmail.com>
2021-03-03 22:22:33 +0000

    added swagger, works with get method

--- src/main/java/com/daru/springrest/controller/Controller.java
+++ src/main/java/com/daru/springrest/controller/Controller.java
@@ -1,15 +1,11 @@
 package com.daru.springrest.controller;
 
 import com.daru.springrest.response.ResponseJson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
-    @Autowired
-    ResponseJson responseJson;
 
     @GetMapping("/employees")
     public ResponseJson getMapping(){
